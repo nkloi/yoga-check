08e8e0e066a04e760fdc26e50049034f61ef8da2
nkloi <nkloi@hcmut.edu.vn>
2022-08-02 23:57:37 +0700

    update

--- main.py
+++ main.py
@@ -21,24 +21,24 @@ STANDARD_ANGLE_POSE = []
 # T_POSE = {'elbow_right' : [165, 175], 'elbow_left' : [165, 175], 'shoulder_right' : [85, 105], 'shoulder_left' : [85, 105], 'hip_right' :[170, 180], 'hip_left' : [170, 180], 'knee_right' : [170, 180], 'knee_left' : [170, 180]}
 T_POSE = [[160, 175], [160, 175],[70, 105], [70, 105], [170, 180],[170, 180], [170, 180], [170, 180]]
 
-T_POSE_1 = [[160, 180], [160, 180],[80, 105], [80, 105], [170, 180],[170, 180], [170, 180], [170, 180]]
-T_POSE_2 = [[160, 180], [160, 180],[70, 90], [70, 90], [170, 180],[170, 180], [170, 180], [170, 180]]
+T_POSE_1 = [[160, 170], [160, 180],[80, 105], [80, 105], [170, 180],[170, 180], [170, 180], [170, 180]]
+T_POSE_2 = [[170, 180], [160, 180],[70, 90], [70, 90], [170, 180],[170, 180], [170, 180], [170, 180]]
 T_POSE_3 = [[160, 180], [160, 180],[60, 80], [60, 80], [170, 180],[170, 180], [170, 180], [170, 180]]
 T_POSE_4 = [[160, 180], [160, 180],[50, 70], [50, 70], [170, 180],[170, 180], [170, 180], [170, 180]]
-T_POSE_LIST = [T_POSE_1, T_POSE_2, T_POSE_3, T_POSE_4]
+FORWARD_BAND_LIST = [T_POSE_1, T_POSE_2, T_POSE_3, T_POSE_4]
 
-FORWARD_BAND_1 = [[160, 180], [160, 180],[100, 170], [100, 170], [15, 30],[15, 30], [160, 180], [160, 180]]
-FORWARD_BAND_2 = [[160, 180], [160, 180],[100, 170], [100, 170], [30, 40],[30, 40], [160, 180], [160, 180]]
-FORWARD_BAND_3 = [[160, 180], [160, 180],[100, 170], [100, 170], [40, 55],[40, 55], [160, 180], [160, 180]]
-FORWARD_BAND_4 = [[160, 180], [160, 180],[100, 170], [100, 170], [55, 100],[55, 100], [160, 180], [160, 180]]
+# FORWARD_BAND_1 = [[160, 180], [160, 180],[100, 170], [100, 170], [15, 30],[15, 30], [160, 180], [160, 180]]
+FORWARD_BAND_1 = [[160, 170], [160, 170],[130, 140], [130, 140], [15, 25],[15, 25], [165, 180], [170, 185]]
+FORWARD_BAND_2 = [[170, 180], [165, 175],[140, 160], [140, 160], [15, 30],[15, 30], [170, 185], [170, 185]]
+FORWARD_BAND_3 = [[160, 180], [170, 180],[105, 120], [105, 120], [45, 55],[40, 55], [160, 175], [155, 170]]
+FORWARD_BAND_4 = [[160, 170], [160, 170],[50, 70], [50, 70], [70, 90],[70, 90], [170, 185], [165, 180]]
 FORWARD_BAND_LIST = [FORWARD_BAND_1, FORWARD_BAND_2, FORWARD_BAND_3, FORWARD_BAND_4]
 
 
-# IMAGE_FILES.append('D:\Danh AI\MyProject\Yoga\Yogacheck\Image\yoga_easy.jpg')  ## Not FORWARD BEND - 0
-# IMAGE_FILES.append('D:\Danh AI\MyProject\Yoga\Yogacheck\Image\Danh.png')  ## Not FORWARD BEND - 0
-IMAGE_FILES.append('D:\Danh AI\MyProject\Yoga\Yogacheck\Image\i_t_pose.jpg')  ## Not FORWARD BEND - 0
-# IMAGE_FILES.append('D:\Danh AI\MyProject\Yoga\Yogacheck\Image\i_forwardbend.jpg') ## FORWARD BEND - 500
-# IMAGE_FILES.append('D:\Danh AI\MyProject\Yoga\Yogacheck\Image\i_forwardbend2.jpg') ## FORWARD BEND - 250
+IMAGE_FILES.append('image\\forwardBend125.jpg')
+IMAGE_FILES.append('image\\forwardBend200.jpg')
+IMAGE_FILES.append('image\\forwardBend375.jpg')
+IMAGE_FILES.append('image\\forwardBend500.jpg')
 
 BG_COLOR = (192, 192, 192)  # gray
 LIST_POST = ["NOSE", "LEFT_EYE_INNER", "LEFT_EYE", "LEFT_EYE_OUTER", "RIGHT_EYE_INNER", "RIGHT_EYE", "RIGHT_EYE_OUTER",
@@ -70,7 +70,7 @@ def is_pose_in_POSTLIST(list_angle, POSE):
     return is_pose_in
 
 def pose_detection(list_angle, POSE_LIST):
-    mask_list = [500, 375, 250, 125]
+    mask_list = [500, 375, 200, 125]
     # is_yoga_pose = "FORWARD BEND"
     is_yoga_pose = "T POSE"
     mask = 125
@@ -155,7 +155,7 @@ if __name__ == '__main__':
             
             lic1 = [angle_1, angle_2, angle_3, angle_4, angle_5, angle_6, angle_7, angle_8]
             print(lic1)
-            is_yoga_pose, color, mask = pose_detection(lic1, T_POSE_LIST)
+            is_yoga_pose, color, mask = pose_detection(lic1, FORWARD_BAND_LIST)
             print(is_yoga_pose)
 
             # Print Pose_detection
@@ -195,84 +195,84 @@ if __name__ == '__main__':
                 results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
 
     # For webcam input:
-    cap = cv2.VideoCapture(0)
-    with mp_pose.Pose(
-            min_detection_confidence=0.5,
-            min_tracking_confidence=0.5) as pose:
-        while cap.isOpened():
-            success, image = cap.read()
-            if not success:
-                print("Ignoring empty camera frame.")
-                # If loading a video, use 'break' instead of 'continue'.
-                continue
-
-            # To improve performance, optionally mark the image as not writeable to
-            # pass by reference.
-            image.flags.writeable = False
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            image = cv2.flip(image, 1)
-            results = pose.process(image)
-            image_rows, image_cols, _ = image.shape
-
-            lic = {'angle_1': 'elbow_right', 'angle_2': 'elbow_left', 'angle_3': 'shoulder_right', 'angle_4': 'shoulder_left', 'angle_5': 'hip_right', 'angle_6': 'hip_left', 'angle_7': 'knee_right', 'angle_8':'knee_left'}
-            for idx, landmark in enumerate(results.pose_landmarks.landmark):
-                idx_to_coordinates = {}
-                if ((landmark.HasField('visibility') and
-                     landmark.visibility < VISIBILITY_THRESHOLD) or
-                        (landmark.HasField('presence') and
-                         landmark.presence < PRESENCE_THRESHOLD)):
-                    continue
-
-                landmark_px = normalized_to_pixel_coordinates(landmark.x, landmark.y, image_cols, image_rows)
-                landmarks = results.pose_landmarks.landmark
-
-                # Get coordinates
-                shoulder_right = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
-                elbow_right = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
-                wrist_right = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
-                shoulder_left = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-                hip_left = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
-                ankle_left = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
-                hip_right = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
-                ankle_right = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
-                knee_right = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
-                knee_left = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
-                elbow_left = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
-                wrist_left = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
-                # Calculate angle
-                angle_1 = calculate_angle(shoulder_right, elbow_right, wrist_right)
-                angle_2 = calculate_angle(shoulder_left, elbow_left, wrist_left)
-                angle_3 = calculate_angle(elbow_right, shoulder_right, hip_right)
-                angle_4 = calculate_angle(elbow_left, shoulder_left,hip_left)
-                angle_5 = calculate_angle(shoulder_right, hip_right, knee_right)
-                angle_6 = calculate_angle(shoulder_left, hip_left, knee_left)
-                angle_7 = calculate_angle(hip_right, knee_right, ankle_right)
-                angle_8 = calculate_angle(hip_left, knee_left, ankle_left)
-                
-                lic1 = [angle_1, angle_2, angle_3, angle_4, angle_5, angle_6, angle_7, angle_8]
-                lic2 = [elbow_right, elbow_left, shoulder_right,shoulder_left, hip_right, hip_left, knee_right, knee_left ]
-                # Visualize angle
-                # lic = round([angle_1, angle_2, angle_3, angle_4, angle_5,angle_6,angle_7,angle_8],2)
-                # lic = {'angle_1': 'elbow_right', 'angle_2': 'elbow_left', 'angle_3': 'shoulder_right', 'angle_4': 'shoulder_left', 'angle_5': 'hip_right', 'angle_6': 'hip_left', 'angle_7': 'knee_right', 'angle_8':'knee_left'}
-                print(lic1)
-                is_yoga_pose, color, mask = pose_detection(lic1, T_POSE_LIST)
-                print(is_yoga_pose)
-                cv2.putText(image, is_yoga_pose + str(": ") + str(mask), (10, 30),cv2.FONT_HERSHEY_PLAIN, 2, color, 2)
-                for i in range(len(lic1)):
-                    cv2.putText(image, str(round(lic1[i],2)), 
-                                tuple(np.multiply(lic2[i], [640, 480]).astype(int)), 
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA
-                                        )          
-            # Draw the pose annotation on the image.
-            image.flags.writeable = True
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-            mp_drawing.draw_landmarks(
-                image,
-                results.pose_landmarks,
-                mp_pose.POSE_CONNECTIONS,
-                landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-            # Flip the image horizontally for a selfie-view display.
-            cv2.imshow('MediaPipe Pose',image)
-            if cv2.waitKey(5) & 0xFF == ord('q'):
-                break
-    cap.release()
+    # cap = cv2.VideoCapture(0)
+    # with mp_pose.Pose(
+    #         min_detection_confidence=0.5,
+    #         min_tracking_confidence=0.5) as pose:
+    #     while cap.isOpened():
+    #         success, image = cap.read()
+    #         if not success:
+    #             print("Ignoring empty camera frame.")
+    #             # If loading a video, use 'break' instead of 'continue'.
+    #             continue
+    #
+    #         # To improve performance, optionally mark the image as not writeable to
+    #         # pass by reference.
+    #         image.flags.writeable = False
+    #         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    #         image = cv2.flip(image, 1)
+    #         results = pose.process(image)
+    #         image_rows, image_cols, _ = image.shape
+    #
+    #         lic = {'angle_1': 'elbow_right', 'angle_2': 'elbow_left', 'angle_3': 'shoulder_right', 'angle_4': 'shoulder_left', 'angle_5': 'hip_right', 'angle_6': 'hip_left', 'angle_7': 'knee_right', 'angle_8':'knee_left'}
+    #         for idx, landmark in enumerate(results.pose_landmarks.landmark):
+    #             idx_to_coordinates = {}
+    #             if ((landmark.HasField('visibility') and
+    #                  landmark.visibility < VISIBILITY_THRESHOLD) or
+    #                     (landmark.HasField('presence') and
+    #                      landmark.presence < PRESENCE_THRESHOLD)):
+    #                 continue
+    #
+    #             landmark_px = normalized_to_pixel_coordinates(landmark.x, landmark.y, image_cols, image_rows)
+    #             landmarks = results.pose_landmarks.landmark
+    #
+    #             # Get coordinates
+    #             shoulder_right = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+    #             elbow_right = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+    #             wrist_right = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+    #             shoulder_left = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+    #             hip_left = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+    #             ankle_left = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
+    #             hip_right = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+    #             ankle_right = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
+    #             knee_right = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
+    #             knee_left = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
+    #             elbow_left = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+    #             wrist_left = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+    #             # Calculate angle
+    #             angle_1 = calculate_angle(shoulder_right, elbow_right, wrist_right)
+    #             angle_2 = calculate_angle(shoulder_left, elbow_left, wrist_left)
+    #             angle_3 = calculate_angle(elbow_right, shoulder_right, hip_right)
+    #             angle_4 = calculate_angle(elbow_left, shoulder_left,hip_left)
+    #             angle_5 = calculate_angle(shoulder_right, hip_right, knee_right)
+    #             angle_6 = calculate_angle(shoulder_left, hip_left, knee_left)
+    #             angle_7 = calculate_angle(hip_right, knee_right, ankle_right)
+    #             angle_8 = calculate_angle(hip_left, knee_left, ankle_left)
+    #
+    #             lic1 = [angle_1, angle_2, angle_3, angle_4, angle_5, angle_6, angle_7, angle_8]
+    #             lic2 = [elbow_right, elbow_left, shoulder_right,shoulder_left, hip_right, hip_left, knee_right, knee_left ]
+    #             # Visualize angle
+    #             # lic = round([angle_1, angle_2, angle_3, angle_4, angle_5,angle_6,angle_7,angle_8],2)
+    #             # lic = {'angle_1': 'elbow_right', 'angle_2': 'elbow_left', 'angle_3': 'shoulder_right', 'angle_4': 'shoulder_left', 'angle_5': 'hip_right', 'angle_6': 'hip_left', 'angle_7': 'knee_right', 'angle_8':'knee_left'}
+    #             print(lic1)
+    #             is_yoga_pose, color, mask = pose_detection(lic1, FORWARD_BAND_LIST)
+    #             print(is_yoga_pose)
+    #             cv2.putText(image, is_yoga_pose + str(": ") + str(mask), (10, 30),cv2.FONT_HERSHEY_PLAIN, 2, color, 2)
+    #             for i in range(len(lic1)):
+    #                 cv2.putText(image, str(round(lic1[i],2)),
+    #                             tuple(np.multiply(lic2[i], [640, 480]).astype(int)),
+    #                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA
+    #                                     )
+    #         # Draw the pose annotation on the image.
+    #         image.flags.writeable = True
+    #         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    #         mp_drawing.draw_landmarks(
+    #             image,
+    #             results.pose_landmarks,
+    #             mp_pose.POSE_CONNECTIONS,
+    #             landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+    #         # Flip the image horizontally for a selfie-view display.
+    #         cv2.imshow('MediaPipe Pose',image)
+    #         if cv2.waitKey(5) & 0xFF == ord('q'):
+    #             break
+    # cap.release()
